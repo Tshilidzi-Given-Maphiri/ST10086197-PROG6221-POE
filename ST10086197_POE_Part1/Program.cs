6185c6f1677f475b1edc7f6e2e1bc2e9cc194d87
Tshilidzi-Given-Maphiri <104319025+Tshilidzi-Given-Maphiri@users.noreply.github.com>
2023-04-22 20:14:49 +0200

    Add files via upload

---
+++ ST10086197_POE_Part1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    private int numIngredients;
+    private string[] ingredientNames;
+    private double[] ingredientQuantities;
+    private string[] ingredientUnits;
+    private int numSteps;
+    private string[] steps;
+
+    public Program()
+    {
+        numIngredients = 0;
+        numSteps = 0;
+    }
+
+    public void SetIngredients(int numIngredients)
+    {
+        this.numIngredients = numIngredients;
+        ingredientNames = new string[numIngredients];
+        ingredientQuantities = new double[numIngredients];
+        ingredientUnits = new string[numIngredients];
+
+        for (int i = 0; i < numIngredients; i++)
+        {
+            Console.WriteLine("Enter ingredient name:");
+            ingredientNames[i] = Console.ReadLine();
+
+            Console.WriteLine("Enter ingredient quantity:");
+            ingredientQuantities[i] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter ingredient unit of measurement:");
+            ingredientUnits[i] = Console.ReadLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Program recipe = new Program();
+
+        Console.WriteLine("Enter the number of ingredients:");
+        int numIngredients = int.Parse(Console.ReadLine());
+        recipe.SetIngredients(numIngredients);
+
+        
+
+   
+    }
+}
+
